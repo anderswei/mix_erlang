9ea8d62db937c7e7fe14da25360ebeb1f2495540
anders wei <anders.wei@outlook.com>
2021-10-15 16:43:29 +0800

    make xref work

--- lib/mix/tasks/xeref.ex
+++ lib/mix/tasks/xeref.ex
@@ -3,9 +3,15 @@ defmodule Mix.Tasks.Xeref do
 
   def run(_args) do
     Mix.Task.run(:loadpaths)
+    xref_checks = [:undefined_function_calls, :undefined_functions,
+    :locals_not_used, 
+    :deprecated_function_calls, :deprecated_functions]
 
     {:ok, pid} = :xref.start(xref_mode: :functions)
-
-    IO.inspect(:xref.analyze(pid, :deprecated_functions))
+    :xref.set_library_path(pid, :code.get_path)
+    :xref.set_default(pid, [warnings: true, verbose: true])
+    :xref.add_directory(pid, String.to_charlist(Mix.Project.compile_path))
+    result = Enum.map(xref_checks, fn check -> :xref.analyze(pid, check) end)
+    IO.inspect(result)
   end
 end
